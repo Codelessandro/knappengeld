8c3bfe4573b5bc7515c74ed36be2e0c1118fc812
Alessandro Schneider <alessandro.schneider@campus.tu-berlin.de>
2020-09-15 12:27:47 +0200

    first steps dataLoader

--- data_loader.py
+++ data_loader.py
@@ -1,5 +1,5 @@
-import numpy as np
 
+'''
 class Match():
 
     def get_random_10_values():
@@ -19,12 +19,33 @@ class Match():
 
 
     def y(self):
+'''
 
 
-class DataLoader():
-    def __init__(self, nr_games):
-        games = map(lambda game: Match(game), np.arange(nr_games));
+def load_game(number):
+    _file = 'database/game' + str(number) + '.txt'
+
+    content=open(_file).read()
+    '''
+    m1 = content.split("Name M2")[0].split("Name M1")[1].split(":")[1]
+    m2 = 
+    schierdsrichter = 
+    m1_positions =
+    m2_positions = 
+    last_results = 
+    player_m1 = 
+    player_m2 = 
+    distance_m1 = 
+    distance_m2 =
+    '''
+
+    results = [
+        m1, m2, schiedsrichter, m1_positions,...
+    ]
+
+    return results;
 
 
-dl = DataLoader(20)
+load_game(1)
+
 
